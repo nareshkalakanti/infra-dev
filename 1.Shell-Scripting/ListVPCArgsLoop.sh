b538da6aeb503c03a1c64662d3c1f7b63738b73b
Naresh K <naresh.kalakanti@gmail.com>
2026-02-07 00:10:00 +0000

    Fix variable usage in ListVPCArgsLoop.sh and clean up prove.sh

--- 1.Shell-Scripting/ListVPCArgsLoop.sh
+++ 1.Shell-Scripting/ListVPCArgsLoop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 REGIONS=$@
-for REGION in REGIONS; do
+for REGION in $REGIONS; do
+  echo "---------------${REGION}-------------"
   aws ec2 describe-vpcs --region $REGION | jq ".Vpcs[].VpcId"
 done
