11d0d7fa203be5345481cfd197be8317b3e80fa1
Naresh K <naresh.kalakanti@gmail.com>
2026-02-09 11:16:58 +0000

    Fix syntax error in nginx service check script

--- 1.Shell-Scripting/nginx.sh
+++ 1.Shell-Scripting/nginx.sh
@@ -1,7 +1,7 @@
 # Check if nginx service is running if not restart the service
 #!/bin/bash
 service nginx status
-if[ $? -eq 0 ];then
+if [ $? -eq 0 ]; then
   echo "nginx server is running"
 else
   echo "lets start nginx service"
